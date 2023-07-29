8cff88d8fc11028eeba9448638c2e66abae7af3a
Dinobi <architectstan01@gmail.com>
2023-07-29 22:38:38 +0100

    Interview Questions

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+"""
+A function that returns a list of lists of integers representing the Pascal's triangle of n
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of Integers representing
+    the Pascal's triangle
+    """
+    mylist = [[1]]
+
+    if n <= 0:
+        return mylist
+    #for i in mylist:
+    #    mylist.append([])
+    #    mylist[i].append(1)
+    for i in range(n - 1):
+        tmp =  [0] + mylist[-1] + [0]
+        row = []
+        for j in range(len(mylist[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
+        mylist.append(row)
+    return mylist
