e47398ea7767ee0706716d8f8112bc61c2272585
Dinobi <architectstan01@gmail.com>
2023-09-30 10:54:19 +0100

    island perimeter task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Island perimeter task"""
+
+def island_perimeter(grid):
+    """Gets the perimeter og a cell in a grid"""
+    if not grid:
+        return 
+    sum = 0
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == 1:
+                sum += 4
+                if x > 0 and grid[x - 1][y] == 1:
+                    sum -= 2
+                if y > 0 and grid[x][y - 1] == 1:
+                    sum -= 2
+    return sum
