8c9365c591b09bf1eb74a21957bc96fdff16bf92
Dinobi <architectstan01@gmail.com>
2023-10-22 06:30:59 +0100

    Interview question

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rotating a 2D matrix at 90 degrees clockwise"""
+
+def rotate_2d_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for count in range(len(matrix)):
+        matrix[count].reverse()
+    return matrix
