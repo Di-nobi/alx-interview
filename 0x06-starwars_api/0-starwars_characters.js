0cec0e4b7944d254af464dd0b1eb4f2637b23026
Dinobi <architectstan01@gmail.com>
2023-10-22 05:46:36 +0100

    Starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const api = 'https://swapi-api.hbtn.io/api';
+const api = 'https://swapi-api.hbtn.io/api/films';
 request(api + process.argv[2], (err, res, body) => {
     if (err) {
         console.log(err);
