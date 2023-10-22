ec22f7562f94b459d0d050d2423d52e0c80b3547
Dinobi <architectstan01@gmail.com>
2023-10-22 06:10:40 +0100

    Starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,64 @@
 #!/usr/bin/node
+// const request = require('request');
+
+// function getData(url) {
+//     return new Promise((resolve, reject) => {
+//         request(url, (err, res, body) => {
+//             if (err) {
+//                 reject(err);
+//             } else if (res.statusCode !== 200) {
+//                 reject( Error('Error'));
+//             } else{
+//             resolve(JSON.parse(body));
+//             }
+//         });
+//     });
+// }
+// function getchar(char) {
+//     const promise = char.map(charUrl => getData(charUrl));
+//     return Promise.all(promise);
+// }
+// request('https://swapi-api.hbtn.io/api/films' + process.argv[2], (err, res, body) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     const char = JSON.parse(body).characters;
+//     getchar(char)
+//     .then(charData => {
+//         console.log(charData);
+//     }).catch(error => {
+//         console.log(error);
+//     });
+// });
+
+#!/usr/bin/node
 const request = require('request');
 
-function getData(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (err, res, body) => {
-            if (err) {
-                reject(err);
-            } else if (res.statusCode !== 200) {
-                reject( Error('Error'));
-            } else{
-            resolve(JSON.parse(body));
-            }
-        });
-    });
-}
-function getchar(char) {
-    const promise = char.map(charUrl => getData(charUrl));
-    return Promise.all(promise);
-}
-request('https://swapi-api.hbtn.io/api/films' + process.argv[2], (err, res, body) => {
-    if (err) {
-        console.log(err);
-    }
-    const char = JSON.parse(body).characters;
-    getchar(char)
-    .then(charData => {
-        console.log(charData);
-    }).catch(error => {
-        console.log(error);
-    });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    // for (const character of characters) {
+    //   request(character, function (error, response, body) {
+    //     if (error) {
+    //       console.log(error);
+    //     } else {
+    //       console.log(JSON.parse(body).name);
+    //     }
+    //   });
+    // }
+    printCharacters(characters, 0);
+  }
 });
 
+function printCharacters (characters, index) {
+  request(characters[index], function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
