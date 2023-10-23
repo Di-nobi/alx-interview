46f75c026dd1602216f3817444b5433f3be5f3e4
Dinobi <architectstan01@gmail.com>
2023-10-23 06:10:39 +0100

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,4 +3,12 @@
 from typing import List
 def makeChange(coins: List, total):
     """Returns fewest number of coins needed to meet the total"""
-    
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    num_coins = 0
+    for i in coins:
+        while total >= i:
+            total -= i
+            num_coins += 1
+    return num_coins if total == 0 else -1
