da134fb0f2a716f9b2eba75b2b774d7aab8badef
Dinobi <architectstan01@gmail.com>
2023-10-30 09:01:44 +0100

    Interview Questions

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """Minimum Operations Interview question"""
+import math
 
+def factors(n):
+    """Gets the factors"""
+    myList = list()
+    for i in range(2, int(math.sqrt(n)) + 1):
+        while n % i == 0:
+            myList.append(i)
+            n //= i
+    if n > 2:
+        myList.append(n)
+    return myList
 def minOperations(n):
     """A method that calculates the fewest number of 
     operations needed to result in n H"""
-    count_move = 0
-    counter = 2
-    while n > 1:
-        while n % 2 == 0:
-            count_move += counter
-            n //= counter
-        counter = counter + 1
-    return count_move
+    if not isinstance(n, int) or n < 2:
+        return 0
+    return sum(factors(n))
