1b9ab84421563215c7f97fe693eb82cb73b49cda
Dinobi <architectstan01@gmail.com>
2023-11-07 14:58:52 +0100

    interview question

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """Island perimeter task"""
 
-
 def island_perimeter(grid):
     """Gets the perimeter og a cell in a grid"""
-    if not grid:
-        return
-    sum = 0
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-    for x in range(len(grid)):
-        for y in range(len(grid)):
-            if grid[x][y] == 1:
-                sum += 4
-                if x > 0 and grid[x - 1][y] == 1:
-                    sum -= 2
-                if y > 0 and grid[x][y - 1] == 1:
-                    sum -= 2
-    return sum
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                neighbors = 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    neighbors -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    neighbors -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    neighbors -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    neighbors -= 1
+                perimeter += neighbors
+    return perimeter
