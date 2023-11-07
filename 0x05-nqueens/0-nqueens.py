869f02075f0ad1f34135f7192ffeaa19236f8a1d
Dinobi <architectstan01@gmail.com>
2023-11-07 14:13:30 +0100

    interview question

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,23 +1,61 @@
 #!/usr/bin/python3
 """Gets the number of queens on the board"""
 
-def isvalid(board, row, col):
-    for prev in range(row):
-        if board[prev] == col or board[prev] - prev == col - row or board[prev] + prev == col + row:
+import sys
+
+def is_safe(board, row, col, number):
+    """Checks if the board is safe"""
+    for i in range(col):
+        if board[i] == row or \
+           board[i] - i == row - col or \
+           board[i] + i == row + col:
             return False
     return True
 
-def solve_nqueens(n):
-    def backtracking(row):
-        if row == n:
-            sol.append(board[:])
-            return
-        for col in range(n):
-            if isvalid(board, row, col):
-                board[row] = col
-                backtracking(row + 1)
-                board[row] = -1
-    board =[-1] * n
-    sol = []
-    backtracking(0)
-    return sol
+def print_board(board):
+    """Prints out the board"""
+    print([[i, board[i]] for i in range(len(board))])
+
+def solve_nqueens_util(board, col, number):
+    """Solves for the queen"""
+    if col == number:
+        print_board(board)
+        return True
+
+    res = False
+    for i in range(number):
+        if is_safe(board, i, col, number):
+            board[col] = i
+            res = solve_nqueens_util(board, col + 1, number) or res
+            board[col] = -1
+
+    return res
+
+def solve_nqueens(number):
+    """Solves for the queen"""
+    board = [-1] * number
+
+    if not solve_nqueens_util(board, 0, number):
+        return False
+
+    return True
+
+def validate(args):
+    """Validates the arguments"""
+    if len(args) == 2:
+        try:
+            number = int(args[1])
+            if number < 4:
+                raise ValueError("N must be at least 4")
+            return number
+        except ValueError:
+            print("N must be a number")
+            exit(1)
+    else:
+        print("Usage: nqueens N")
+        exit(1)
+
+if __name__ == "__main__":
+    """Execution"""
+    number = validate(sys.argv)
+    solve_nqueens(number)
